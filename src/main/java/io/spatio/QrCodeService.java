5c4babc44e579eb23305b25a6d1438482ac81dad
Steve Kabore <fabricestevekabore@gmail.com>
2026-01-06 13:56:13 +0000

    second commit of spatio.io test de spatio avec generateyr de qr . Ajout de base de données sqlite. Ajout de certificat Qr code

--- src/main/java/io/spatio/QrCodeService.java
+++ src/main/java/io/spatio/QrCodeService.java
@@ -5,6 +5,8 @@ import com.google.zxing.common.BitMatrix;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 
 import java.awt.image.BufferedImage;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class QrCodeService {
 
@@ -18,6 +20,18 @@ public class QrCodeService {
         } catch (Exception e) {
             throw new RuntimeException("Erreur génération QR Code", e);
         }
+    }
+    public static BufferedImage generateCertificatQrCode() throws Exception {
+        BufferedImage qrCodeImage = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        SpatioIO spatio = new SpatioIO();
+        SpatioCertificate certificat = spatio.generateFromIP();
+        qrCodeImage= generate(certificat.getHash(), qrCodeImage.getWidth(), qrCodeImage.getHeight());
+        // sauvegarder dans la base
+        SpatioDatabase db = new SpatioDatabase();
+        db.insertCertificate(certificat);
+        // fin de sauvegarde
+        ImageUtil.saveAsPng100x100(qrCodeImage, "spatio-"+ certificat.getHash().substring(0,6)+".png");
+        return qrCodeImage;
     }
 
 }
