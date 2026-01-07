9587c20a059bb38a1d85c452342d3f55d91a7a83
Steve Kabore <fabricestevekabore@gmail.com>
2026-01-07 10:07:30 +0000

    second commit of spatio.io premier test réussie dans un projet de spatio avec generateur de qr 07/01/2026.

--- src/main/java/io/spatio/SpatioDatabase.java
+++ src/main/java/io/spatio/SpatioDatabase.java
@@ -1,14 +1,29 @@
 package io.spatio;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.*;
 
 public class SpatioDatabase {
 
     private static final String DB_URL = "jdbc:sqlite:spatio.db";
-
+    private static SpatioDatabase INSTANCE;
+    private final Connection connection;
     public SpatioDatabase() {
+
+
         try (Connection connection = DriverManager.getConnection(DB_URL)) {
+            Path dir = Paths.get(System.getProperty("user.home"), ".spatio");
+            Files.createDirectories(dir);
+
+            Path dbFile = dir.resolve("spatio.db");
+            String url = "jdbc:sqlite:" + dbFile.toAbsolutePath();
+
+            this.connection = DriverManager.getConnection(url);
+            this.connection.setAutoCommit(true);
             Statement stmt = connection.createStatement();
             stmt.execute("""
                 CREATE TABLE IF NOT EXISTS certificates (
@@ -21,9 +36,20 @@ public class SpatioDatabase {
             """);
         } catch (SQLException e) {
             throw new RuntimeException("Erreur lors de l'initialisation de la base SQLite", e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
+    public static synchronized SpatioDatabase getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SpatioDatabase();
+        }
+        return INSTANCE;
+    }
 
+    public Connection getConnection() {
+        return connection;
+    }
     public void insertCertificate(SpatioCertificate cert) {
         try (Connection conn = DriverManager.getConnection(DB_URL)) {
             String sql = "INSERT OR REPLACE INTO certificates (hash, latitude, longitude, timestamp, signature) VALUES (?, ?, ?, ?, ?)";
@@ -38,8 +64,21 @@ public class SpatioDatabase {
             throw new RuntimeException("Erreur insertion certificat", e);
         }
     }
-
-    public SpatioCertificate getCertificateByHash(String hash) {
+    public int getCount() {
+        try (Connection conn = DriverManager.getConnection(DB_URL)) {
+            String sql = "SELECT COUNT(*) AS nb FROM certificates";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+            return rs.getInt("nb");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            return 0;
+        }
+    }
+        public SpatioCertificate getCertificateByHash(String hash) {
         try (Connection conn = DriverManager.getConnection(DB_URL)) {
             String sql = "SELECT * FROM certificates WHERE hash = ?";
             PreparedStatement stmt = conn.prepareStatement(sql);
